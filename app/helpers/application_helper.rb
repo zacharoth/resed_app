bc35306cdb9448fb3a53a35df8187f7bdeae1542
zacharoth <zachary.roth8@gmail.com>
2013-04-01 22:53:54 -0500

    add full title bar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # returns full title of particular page
+  def full_title(page_title)
+	base_title = "Vanderbilt University | Residential Education | Nashville Event Finder"
+	if page_title.empty?
+	  base_title
+	else 
+	  "#{base_title} | #{page_title}"
+	end
+  end
+
 end
