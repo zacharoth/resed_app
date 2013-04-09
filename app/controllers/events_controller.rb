44bffb259af82a3aae8c7c128e9749e54eb513ab
zacharoth <zachary.roth8@gmail.com>
2013-04-08 23:50:41 -0500

    add events index, refactor event details form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -29,4 +29,29 @@ class EventsController < ApplicationController
 	  render 'new'
 	end
   end
+
+  def edit
+	@event = Event.find(params[:id])
+  end
+
+  def update
+	@event = Event.find(params[:id])
+	if @event.update_attributes(params[:event])
+	  @event.approved = signed_in?
+	  msg = String.new
+	  if signed_in?
+		msg = "Event details updated"
+	  else
+		msg = "Event update suggested"
+	  end
+	  flash[:success] = msg
+	  redirect_to @event
+	else
+	  render 'edit'
+	end
+  end
+
+  def index
+	@events = Event.paginate(page: params[:page])
+  end
 end
